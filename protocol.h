aea85fc52f7ab0c6d004bf44eae846da266b9599
digicontributer <esot@digibyte.io>
2018-03-23 11:35:12 +1000

    update with testnet params

--- protocol.h
+++ protocol.h
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 19333 : 12024;
+    return testnet ? 12025 : 12024;
 }
 
 //
