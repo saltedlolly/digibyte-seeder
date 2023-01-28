5af3a61ad8cef8442b60541620324148f077aced
j50ng <69596007+j50ng@users.noreply.github.com>
2023-01-27 21:44:32 -0500

    Added the correct active mainnet and testnet seeders to main.cpp and change in protocol.h the port to the correct testnet port.

--- protocol.h
+++ protocol.h
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 12025 : 12024;
+    return testnet ? 12026 : 12024;
 }
 
 //
